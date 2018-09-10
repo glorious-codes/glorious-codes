5d2c0cae8cdefe653946715f0408d3784c2b27e9
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-10 00:42:20 -0300

    Using app.js instead index.js as entry point

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,19 @@
 import '@styles/_native.styl'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import router from './router'
 import template from './app.html'
 
-export default {
+const app = {
   name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to your prerender-spa-plugin Vuejs 2.0 demo app!'
-    }
-  },
   template
-}
+};
+
+new Vue({
+  el: '[data-app]',
+  router,
+  render: h => h(app),
+  mounted () {
+    document.dispatchEvent(new Event('render-event'))
+  }
+})
