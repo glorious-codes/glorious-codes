f8ebec634927761ea188590fe2a381894f0f5801
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-10 21:02:26 -0300

    Handling page head with seo service

--- src/scripts/author/views/author.js
+++ src/scripts/author/views/author.js
@@ -1,6 +1,17 @@
+import seoService from '@scripts/base/services/seo/seo';
 import template from './author.html'
+
+const head = seoService.buildHead({
+  title: 'Author',
+  description: [
+    'My name is Rafael Camargo, developer with a sharp eye for design focused',
+    'on Frontend/UI Architecture. Maker of Nashios, Teleflik and Dollert.'
+  ],
+  keywords: 'Rafael Camargo, nashios, teleflik, dollert, frontend, UI'
+});
 
 export default {
   name: 'author',
+  head,
   template
 }
--- src/scripts/home/views/home.js
+++ src/scripts/home/views/home.js
@@ -1,10 +1,21 @@
-import logo from '@scripts/base/components/logo/logo'
-import template from './home.html'
+import seoService from '@scripts/base/services/seo/seo';
+import logo from '@scripts/base/components/logo/logo';
+import template from './home.html';
+
+const head = seoService.buildHead({
+  title: 'Glorious Codes',
+  description: [
+    'Glorious Codes is a project that values clean code and continuous',
+    'refactoring as a way of getting high quality software.'
+  ],
+  keywords: 'npm, packages, glorious, demo, cookie'
+});
 
 export default {
   name: 'home',
   components: {
     logo
   },
+  head,
   template
 }
