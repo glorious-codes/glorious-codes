9d917f874fb9643972beb825ce93d826d3ee4062
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-10 21:01:32 -0300

    Minor improvement on environment variables

--- environments/development.js
+++ environments/development.js
@@ -1,4 +1,7 @@
 export default {
+  APP: {
+    BASE_URL: 'http://localhost:7000'
+  },
   GOOGLE_ANALYTICS: {
     ID: ''
   }
--- environments/production.js
+++ environments/production.js
@@ -1,4 +1,7 @@
 export default {
+  APP: {
+    BASE_URL: 'https://glorious.codes'
+  },
   GOOGLE_ANALYTICS: {
     ID: ''
   }
