9a208a2e0b652da8f5298f5b712d6c558e5342c1
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-10 21:01:18 -0300

    Created seo service

---
+++ src/scripts/base/services/seo/seo.js
@@ -0,0 +1,31 @@
+import ENV from '@environment';
+
+const _public = {};
+
+_public.buildHead = ({ title, description, keywords }) => {
+  const desc = Array.isArray(description) ? description.join(' ') : description;
+  const imageUrl = `${ENV.APP.BASE_URL}/images/glorious-codes-1012x508.png`;
+  return {
+    title: {
+      inner: title
+    },
+    meta: [
+      { name: 'application-name', content: '' },
+      { name: 'description', content: desc, id: 'desc' },
+      { name: 'keywords', content: keywords },
+      // Twitter
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:desc', content: desc },
+      { name: 'twitter:site', content: ENV.APP.BASE_URL },
+      { name: 'twitter:creator', content: '@rcamargo' },
+      // Google+ / Schema.org
+      { itemprop: 'name', content: title },
+      { itemprop: 'desc', content: desc },
+      // Facebook / Open Graph
+      { property: 'og:title', content: title },
+      { property: 'og:image', content: imageUrl }
+    ]
+  }
+};
+
+export default _public;
