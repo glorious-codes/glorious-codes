ad7730f3181e4c5c917e27dc49ad31d65ef8d001
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-10 21:02:04 -0300

    Minor improvement on routes

--- src/scripts/routes.js
+++ src/scripts/routes.js
@@ -2,8 +2,16 @@ import home from '@scripts/home/views/home'
 import author from '@scripts/author/views/author'
 
 const routes = [
-  { path: '/', name: 'home', component: home },
-  { path: '/author', name: 'author', component: author }
+  {
+    path: '/',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/author',
+    name: 'author',
+    component: author
+  }
 ];
 
 export default routes;
