bf2251589abd657a0514ff5371404b34d61a17ad
Rafael Camargo <contato@rafaelcamargo.com>
2018-09-19 21:45:14 -0300

    Create codes cookie view

---
+++ src/scripts/codes/views/cookie/cookie.js
@@ -0,0 +1,26 @@
+import seoService from '@scripts/base/services/seo/seo';
+import row from '@scripts/base/components/row/row';
+import rowItem from '@scripts/base/components/row-item/row-item';
+import viewport from '@scripts/base/components/viewport/viewport';
+import codeViewSummary from '@scripts/codes/components/code-view-summary/code-view-summary';
+import cookieDemo from '@scripts/codes/components/cookie-demo/cookie-demo';
+import template from './cookie.html';
+
+const head = seoService.buildHead({
+  title: 'Glorious Cookie',
+  description: 'The tiniest cookies library.',
+  keywords: 'glorious, cookie, browser'
+});
+
+export default {
+  name: 'cookie',
+  components: {
+    row,
+    rowItem,
+    viewport,
+    codeViewSummary,
+    cookieDemo
+  },
+  head,
+  template
+};
---
+++ src/scripts/codes/views/cookie/cookie.test.js
@@ -0,0 +1,44 @@
+import { shallowMount } from '@vue/test-utils';
+import viewport from '@scripts/base/components/viewport/viewport';
+import codeViewSummary from '@scripts/codes/components/code-view-summary/code-view-summary';
+import cookieDemo from '@scripts/codes/components/cookie-demo/cookie-demo';
+import cookie from './cookie';
+
+describe('Codes Cookie View', () => {
+  function mountComponent(){
+    return shallowMount(cookie);
+  }
+
+  it('should contain a viewport', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.contains(viewport)).toEqual(true);
+  });
+
+  it('should contain a vertical centered row', () => {
+    const wrapper = mountComponent();
+    const row = wrapper.find('row-stub').element;
+    expect(row.getAttribute('vertical-centered')).toBeDefined();
+  });
+
+  it('should contain a code view summary', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.contains(codeViewSummary)).toEqual(true);
+  });
+
+  it('should code view summary contain a heading', () => {
+    const wrapper = mountComponent();
+    const viewSummary = wrapper.find('codeviewsummary-stub').element;
+    expect(viewSummary.getAttribute('heading')).toEqual('Glorious Cookie');
+  });
+
+  it('should code view summary contain an external link', () => {
+    const wrapper = mountComponent();
+    const viewSummary = wrapper.find('codeviewsummary-stub').element;
+    expect(viewSummary.getAttribute('externallinkhref')).toEqual('https://github.com/rafaelcamargo/glorious-cookie#install');
+  });
+
+  it('should contain a cookie demo', () => {
+    const wrapper = mountComponent();
+    expect(wrapper.contains(cookieDemo)).toEqual(true);
+  });
+});
