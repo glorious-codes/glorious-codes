264d520e138d669cba04006446842a9c5be62313
Rafael Camargo <contato@rafaelcamargo.com>
2018-10-16 00:48:10 -0300

    Apply code demo toolbar to crud demo component

--- src/scripts/codes/components/crud-demo/crud-demo.js
+++ src/scripts/codes/components/crud-demo/crud-demo.js
@@ -3,11 +3,29 @@ import '@glorious/demo/dist/gdemo.min.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import GDemo from '@glorious/demo';
 import Prism from 'prismjs';
+import ENV from '@environment';
+import baseLink from '@scripts/base/components/base-link/base-link';
+import row from '@scripts/base/components/row/row';
+import rowItem from '@scripts/base/components/row-item/row-item';
+import tweetBtn from '@scripts/base/components/tweet-btn/tweet-btn';
 import dateService from '@scripts/base/services/date/date';
+import codeDemoToolbar from '@scripts/codes/components/code-demo-toolbar/code-demo-toolbar';
 import template from './crud-demo.html';
 
 export default {
-  name: 'cookie-demo',
+  name: 'crud-demo',
+  components: {
+    baseLink,
+    row,
+    rowItem,
+    tweetBtn,
+    codeDemoToolbar
+  },
+  data(){
+    return {
+      tweet: `Just discovered a bare minimum and extensible crud generator! ${ENV.APP.BASE_URL}/crud`
+    };
+  },
   mounted(){
     this.play();
   },
