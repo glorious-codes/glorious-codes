b493928bf60d9825c0b40989ba953212d9da32f9
Rafael Camargo <hello@rafaelcamargo.com>
2019-02-03 22:34:03 -0200

    Allow glorious demo live demonstartion handle with braces inside back ticks

--- src/scripts/codes/services/demo-demo-initial-code/demo-demo-initial-code.js
+++ src/scripts/codes/services/demo-demo-initial-code/demo-demo-initial-code.js
@@ -3,17 +3,32 @@ import DEFAULT_DEMO_CODE from '@scripts/codes/constants/demo-demo';
 import routeService from '@scripts/base/services/route/route';
 
 const INITIAL_CODE_QUERY_PARAM = 'demo';
+const OPENING_BRACE_CODE = '%OPBR%';
+const CLOSING_BRACE_CODE = '%CLBR%';
 
 const _public = {};
 
 _public.get = () => {
   const demo = routeService.getQueryParams(INITIAL_CODE_QUERY_PARAM);
-  return demo ? window.atob(demo) : DEFAULT_DEMO_CODE;
+  return demo ? decodeBraces(window.atob(demo)) : DEFAULT_DEMO_CODE;
 };
 
 _public.buildParameterizedUrl = demoCode => {
+  const code = encodeBraces(demoCode);
   const queryParam = INITIAL_CODE_QUERY_PARAM;
-  return `${ENV.APP.BASE_URL}/demo?${queryParam}=${window.btoa(demoCode)}`;
+  return `${ENV.APP.BASE_URL}/demo?${queryParam}=${window.btoa(code)}`;
 };
+
+function encodeBraces(string){
+  return string
+    .replace(/\{/g, OPENING_BRACE_CODE)
+    .replace(/\}/g, CLOSING_BRACE_CODE);
+}
+
+function decodeBraces(string){
+  const opbrRegex = new RegExp(OPENING_BRACE_CODE, 'g');
+  const clbrRegex = new RegExp(CLOSING_BRACE_CODE, 'g');
+  return string.replace(opbrRegex, '{').replace(clbrRegex, '}');
+}
 
 export default _public;
