9c0c43a793c6d2b1723c0dc33df06fea398e6e2f
Rafael Camargo <hello@rafaelcamargo.com>
2019-10-28 15:30:33 -0300

    feat: add fyzer view

---
+++ src/scripts/codes/views/fyzer/fyzer.js
@@ -0,0 +1,30 @@
+import '@images/fyzer-200x200.png';
+import seoService from '@scripts/base/services/seo/seo';
+import row from '@scripts/base/components/row/row';
+import rowItem from '@scripts/base/components/row-item/row-item';
+import viewport from '@scripts/base/components/viewport/viewport';
+import codeViewSummary from '@scripts/codes/components/code-view-summary/code-view-summary';
+import codeWeightBadge from '@scripts/codes/components/code-weight-badge/code-weight-badge';
+import fyzerDemo from '@scripts/codes/components/fyzer-demo/fyzer-demo';
+import template from './fyzer.html';
+
+const head = seoService.buildHead({
+  title: 'Glorious Fyzer',
+  description: 'The tiniest fyzers library.',
+  keywords: 'glorious, fyzer, page fold',
+  imageFilename: 'fyzer-200x200.png'
+});
+
+export default {
+  name: 'fyzer',
+  components: {
+    row,
+    rowItem,
+    viewport,
+    codeViewSummary,
+    codeWeightBadge,
+    fyzerDemo
+  },
+  head,
+  template
+};
