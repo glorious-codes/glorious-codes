130fa2f1852eac7c2423025a6367d28f85c5862d
Rafael Camargo <hello@rafaelcamargo.com>
2023-09-15 16:27:45 -0300

    chore: config compilorama deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,7 +3,9 @@
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$branch" = "master" ]; then
-    npm run deploy -- --token=$FIREBASE_TOKEN
+  npm run deploy -- --token=$FIREBASE_TOKEN
+elif [ "$branch" = "compilorama" ]; then
+  npm run deploy -- --token=$COMP_FB_TOKEN -P $COMP_FB_PROJECT
 else
-    echo "skipped"
+  echo "skipped"
 fi
